ba15098a4a395c1428809cdcaf7f9c7ab99a499e
Jason Kang <kangjason02@gmail.com>
2023-04-12 19:18:19 -0400

    adding login screen

--- app/src/main/java/com/example/owncheff/LoginActivity.kt
+++ app/src/main/java/com/example/owncheff/LoginActivity.kt
@@ -9,20 +9,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginFragment: Fragment = LoginFragment()
-        val signupFragment: Fragment = SignupFragment()
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.loginFl, loginFragment)
-        fragmentTransaction.commit()
 
 
     }
-    private fun replaceFragment(recipeFragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.loginFl, recipeFragment)
-        fragmentTransaction.commit()
-    }
+
 }
