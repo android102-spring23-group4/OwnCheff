bf97027c88fa849a883970c8b43b7929b8d83c62
Jason Kang <kangjason02@gmail.com>
2023-04-12 19:29:52 -0400

    adding login screen

--- app/src/main/java/com/example/owncheff/LoginActivity.kt
+++ app/src/main/java/com/example/owncheff/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class LoginActivity : AppCompatActivity() {
@@ -15,13 +16,18 @@ class LoginActivity : AppCompatActivity() {
     val usernameEt = findViewById<EditText>(R.id.usernameEt)
     val passwordEt = findViewById<EditText>(R.id.passwordEt)
     val button = findViewById<Button>(R.id.loginBtn)
+    val signupTv = findViewById<TextView>(R.id.signupTv)
 
 
         button.setOnClickListener {
+            //implement some sort of check for account here once db is implemented.
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        signupTv.setOnClickListener {
+            //clicking will go to signup activity
+        }
 
     }
 
