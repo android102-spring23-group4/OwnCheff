8fbe8b9467df9580398bea8c3773d9a82fed8f82
MiguelAmaro <miguelamaro100@gmail.com>
2023-04-18 22:42:22 -0400

    example/test of using the mealdb api

--- app/src/main/java/com/example/owncheff/MainActivity.kt
+++ app/src/main/java/com/example/owncheff/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.owncheff
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.widget.Button
 import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "Main Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,6 +46,27 @@ class MainActivity : AppCompatActivity() {
         val greetingsTV = findViewById<TextView>(R.id.greetingsTV)
         greetingsTV.text = getString(R.string.user_greeting, userName)
         signOutButton.setOnClickListener() { signOut() }
+
+
+        // TEST API REQUEST
+        val ARTICLE_SEARCH_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
+        val client = AsyncHttpClient()
+        client.get(ARTICLE_SEARCH_URL, object : JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "Failed to fetch articles: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i(TAG, "Successfully fetched articles: $json")
+                //json.jsonObject.toString()
+            }
+        })
+        // TEST API REQUEST
     }
 
     // HELPERS
